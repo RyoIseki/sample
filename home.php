e2d97fdab6fc8e8b2eb4254620f95d04a8312573
RyoIseki <lespaulryo@gmail.com>
2015-11-13 01:29:00 +0900

    サンプルコード追加

---
+++ home.php
@@ -0,0 +1,63 @@
+﻿<!DOCTYPE html>
+<html>
+	<head>
+	<title>Password Factory</title>
+	<meta charset="UTF-8">
+	</head>
+	<body>
+	<h3>パスワード生成</h3>
+	<?php
+		require_once('./makePassword.php');
+
+		$passfactory = Password::getInstance();
+		$passfactory->setPassArray(6,6,1);
+		$passValue = $passfactory->getPass();
+
+		if(isset($_POST['number']) && isset($_POST['digit']) && isset($_POST['style']))
+		{
+			$passfactory->setPassArray($_POST['number'],$_POST['digit'],$_POST['style']);
+			$passValue = $passfactory->getPass();
+		}
+	?>
+	<form method="post" action="index.php">
+		<p>個数<br>
+		<input type="radio" name="number" value="6" checked> 6個
+		<input type="radio" name="number" value="8"> 8個
+		<input type="radio" name="number" value="10"> 10個
+		</p>
+
+		<p>桁数<br>
+		<input type="radio" name="digit" value="6" checked> 6個
+		<input type="radio" name="digit" value="8"> 8個
+		<input type="radio" name="digit" value="10"> 10個
+		</p>
+		
+		<p>スタイル<br>
+		<input type="radio" name="style" value="0" checked> 半角英字
+		<input type="radio" name="style" value="1"> 半角英字＆数字
+		<input type="radio" name="style" value="2"> 半角英字(大小)＆数字
+		</p>
+		
+		<p><input type="submit" value="生　成"></p>
+	</form>
+
+		<table>
+			<tr>
+				<?php
+				for($i = 1; $i <= count($passValue); $i++ )
+				{
+					if($i % 3 == 0)
+					{
+						echo '<td><input type="text" name="password" 
+						size="30" maxlength="20" value='.$passValue[$i-1].'></td></tr><tr>';
+					}
+					else
+					{
+						echo '<td><input type="text" name="password" 
+						size="30" maxlength="20" value='.$passValue[$i-1].'></td>';
+					}
+				}
+				?>
+		</table>
+	</body>
+</html>
---
+++ makePassword.php
@@ -0,0 +1,59 @@
+﻿<?php
+
+class Password{
+
+	private static $passInstance;
+	private $passArray;
+	private $passNum;
+	private $passDigit;
+	
+	protected function __construct(){}
+	
+	public static function getInstance()
+	{
+		if(is_null(static::$passInstance))
+		{
+			static::$passInstance = new Password();
+		}
+		return static::$passInstance;
+	}
+
+	public function setPassArray($num,$digit,$style)
+	{
+		$this->passNum = $num;
+		$this->passDigit = $digit;
+
+		if($style == 0)
+		{
+			$source = "abcdefghijklmnopqrstuvwxyz";
+		}
+		else if($style == 1)
+		{
+			$source = "abcdefghijklmnopqrstuvwxyz1234567890";
+		}
+		else if($style == 2)
+		{
+			$source = "abcdefghijklmnopqrstuvwxyz1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+		}
+
+		$array = array();
+		for($i = 0; $i < $this->passNum ; $i++ )
+		{
+			$shuffledStrings = str_shuffle($source);
+			$array[] = substr($shuffledStrings,0,$this->passDigit);
+		}
+		$this->passArray = $array;
+	}
+
+	public function getPass()
+	{
+		return $this->passArray;
+	}
+	
+	//クローン禁止
+	final public function __clone()
+	{
+		throw new Exception("this instance is singleton class.");
+	}
+}
+?>
